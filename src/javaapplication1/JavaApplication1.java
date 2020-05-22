28ff3775f052a7d09641fc7964cade036e9591d7
Andrés <apias@cci.edu.uy>
2020-05-22 15:20:32 -0300

    generando la version 1.0

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,7 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("PRUEBA");
+        System.out.print("cambiamos el mensaje");
     }
     
 }
